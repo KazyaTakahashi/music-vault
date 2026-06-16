767c49e30f668dc84e21653dba1a40756213f2d5
Kazya Takahashi <95883403+QBeeIII@users.noreply.github.com>
2026-06-16 01:22:42 -0400

    Vercel Turso Test

---
+++ src/app/MV/2FA/page.js
@@ -0,0 +1,4 @@
+export default function TwoFAPage()
+{
+  return <p></p>
+}
--- src/app/MV/register/page.js
+++ src/app/MV/register/page.js
@@ -115,6 +115,7 @@ export default function RegisterPage()
   
   return (
     <>
+    <p>{process.env.TEST}</p>
       <form onSubmit={handleSubmit}>
         
         {errors.length > 0 && <ErrorList list={errors} />}
